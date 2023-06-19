13908722114daabadb9281ff4326dcf485a349ca
Gabriel Silva <silvagabriel.df@gmail.com>
2023-06-18 21:43:59 -0300

    alteração do input tel, acréscimo do mask

--- js/main.js
+++ js/main.js
@@ -2,10 +2,11 @@ $(document).ready(function(){
 
     $('#cpf').mask('000.000.000-00', {
         placeholder: 'Digite seu CPF'
-    })
+    });
 
     $('#cep').mask('00000-000', {
         placeholder: 'Digite seu cep'
-    })
+    });
 
-})
+    $('#telefone').mask('(00) 00000-0000');
+});
