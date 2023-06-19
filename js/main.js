8e29e674b3f29bb80907fe057ce90303c6b3f729
Gabriel Silva <silvagabriel.df@gmail.com>
2023-06-18 21:36:10 -0300

    novo commit

---
+++ js/main.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){
+
+    $('#cpf').mask('000.000.000-00', {
+        placeholder: 'Digite seu CPF'
+    })
+
+    $('#cep').mask('00000-000', {
+        placeholder: 'Digite seu cep'
+    })
+
+})
